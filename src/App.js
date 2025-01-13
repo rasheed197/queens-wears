b2e3e6eb5e3d1e64ee50e90f2558aadcf7670a57
tiamiyu rasheed <tiamiyurasheed.tr@gmail.com>
2025-01-13 17:48:09 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Boots</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Flats</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Heels</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Sandals</h2>
+          <p>Shop Now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
